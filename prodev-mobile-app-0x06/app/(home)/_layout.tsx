16fd8f703a4053b90a4193af226001ab06d8438e
LaughterOludu <99695248+LaughterOludu@users.noreply.github.com>
2026-01-21 13:03:07 +0100

    Update _layout.tsx

--- prodev-mobile-app-0x06/app/(home)/_layout.tsx
+++ prodev-mobile-app-0x06/app/(home)/_layout.tsx
@@ -7,7 +7,7 @@ const HomeRootLayout = () => {
   return (
     <Tabs screenOptions={{
         tabBarActiveTintColor: '#34967C',
-        headerShown: false
+       
     }} >
       <Tabs.Screen name="index" options={{
         title: 'Home',
@@ -15,12 +15,10 @@ const HomeRootLayout = () => {
       }} />
       <Tabs.Screen name="search" options={{
         title: 'Search',
-        headerShown: true,
         tabBarIcon: ({ color }) => <Feather name="search" size={24} color={color} />,
       }} />
       <Tabs.Screen name="saved" options={{
         title: 'Saved',
-        headerShown: true,
         tabBarIcon: ({ color }) => <EvilIcons name="heart" size={27} color={color} />
       }} />
         <Tabs.Screen name="inbox" options={{
